b4c3e4ef8bf711804cc00f34cdf582fcb2e69cf8
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-16 16:45:32 +0000

    add feature test for a game with all gutter balls

---
+++ spec/featureSpec.js
@@ -0,0 +1,20 @@
+describe('Feature test: The app', () => {
+  const ScoreCard = require('../src/scoreCard');
+  let scoreCard;
+
+  beforeEach(() => {
+    scoreCard = new ScoreCard();
+  });
+
+  it('can complete a game with 20x gutter balls', () => {
+    for(let i = 0; i < 10; i++){
+      scoreCard.frames[i].rollOne = 0;
+      scoreCard.frames[i].rollTwo = 0;
+    }
+
+    scoreCard.calculateTotalScore();
+    expect(scoreCard.gameScore).toEqual(0)
+
+  });
+
+});
