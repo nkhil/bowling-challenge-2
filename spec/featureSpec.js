99a967c275724c45ae02676602a33ef5b4eb5fb9
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-17 20:27:20 +0000

    add feature test for scoring 3 strikes in a row

--- spec/featureSpec.js
+++ spec/featureSpec.js
@@ -13,9 +13,24 @@ describe('Feature test: The app', () => {
     }
     scoreCard.getFrameScores();
     scoreCard.calculateTotalScore();
-    expect(scoreCard.gameScore).toEqual(0)
-
+    expect(scoreCard.gameScore).toEqual(0);
   });
+
+  it('can score a game with 3 strikes in a row', () => {
+    for(let i = 0; i < 3; i++){  
+      scoreCard.frames[i].rollOne = 10;
+    }
+    scoreCard.frames[0].calculateScore();
+    scoreCard.frames[1].calculateScore();
+    scoreCard.frames[2].calculateScore();
+    expect(scoreCard.frames[0].totalScore).toEqual(30);
+    
+    scoreCard.getFrameScores();
+    scoreCard.calculateTotalScore();
+    expect(scoreCard.gameScore).toEqual(30);
+  });
+
+  // it('can score a game with ')
 
   
 
