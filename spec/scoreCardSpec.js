7a544e86c90f69897b82645323763f613fd87c9b
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-17 19:37:30 +0000

    add tests for getFrameScores() and calculateTotalScore()

---
+++ spec/scoreCardSpec.js
@@ -0,0 +1,32 @@
+describe('ScoreCard', () => {
+  const ScoreCard = require('../src/scoreCard')
+  let scoreCard;
+  let frame;
+
+  beforeEach(() => {
+    scoreCard = new ScoreCard();
+    frame = jasmine.createSpyObj('frame',['calculateScore']);
+
+  });
+
+  describe('getFrameScores()', () => {
+    it('can get the scores from individual frames', () => {
+      (frame.calculateScore).and.returnValue(1);
+      scoreCard.frames = [frame, frame, frame, frame, frame];
+      scoreCard.getFrameScores();
+      scoreCard.calculateTotalScore();
+      expect(scoreCard.gameScore).toEqual(5);  
+    });
+  });
+
+  describe('calculateTotalScore', () => {
+    it('can calculate the total score of a game correctly', () =>{
+      scoreCard.frameScoreArray = [1, 2, 3, 4, 5];
+      scoreCard.calculateTotalScore();
+      expect(scoreCard.gameScore).toEqual(15);
+    })
+  });
+
+  
+
+});
