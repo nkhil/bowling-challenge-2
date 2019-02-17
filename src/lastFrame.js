044ea6e491fa23b0915f61b1ec3677b7f7665029
Nikhil Vijayan <nkhilv@gmail.com>
2019-02-17 23:29:36 +0000

    add LastGame logic

--- src/lastFrame.js
+++ src/lastFrame.js
@@ -9,9 +9,76 @@ class LastFrame {
     this.game = game;
   }
 
-  calculateScore(){
-    return 0;
+  total(){
+    if(this.rollOne === null && this.rollTwo === null){
+      return null;
+    } else {
+      return this.rollOne + this.rollTwo + this.rollThree;
+    }
   }
+
+  isStrike(roll){
+    return this.roll === 10
+  }
+
+  isSpare(roll1, roll2){
+    const rollOne = this.rollOne;
+    const rollTwo = this.rollTwo;
+    return roll1 + roll2 === 10 && rollTwo !== null;
+  }
+
+  rollOneIsStrike(){
+    return this.isStrike(this.rollOne);
+  }
+
+  rollTwoIsStrike(){
+    return this.isStrike(this.rollTwo);
+  }
+
+  rollThreeIsStrike(){
+    return this.isStrike(this.rollThree);
+  }
+
+  framePlayed(){
+    if(this.rollOneIsStrike()){
+      return this.rollTwo != null && this.rollThree != null;
+    } else if(this.isSpare(this.rollOne, this.rollTwo)){
+      return this.rollThree != null;
+    } else {
+      return this.rollOne != null && this.rollTwo != null;
+    }
+  }
+
+  calculateScore(){
+    if(this.framePlayed()) {
+
+      if(this.rollOneIsStrike()){
+        this.handleRollOneStrike();
+      } else if(this.isSpare(this.rollOne, this.rollTwo)){
+        this.handleSpare();
+      } else {
+        this.totalScore = this.total();
+      }
+      
+    }
+    return this.totalScore;
+  }
+
+  handleRollOneStrike(){
+    if(this.rollTwoIsStrike() && this.rollThreeIsStrike()){
+      this.totalScore = 30;
+    } else if(this.rollTwoIsStrike()){
+      this.totalScore = 20 + this.rollThree;
+    } else {
+      this.totalScore = 10 + this.rollTwo + this.rollThree;
+    }
+  }
+
+  handleSpare(){
+    this.totalScore = 10 + this.rollThree;
+  }
+
+
 
 }
 
